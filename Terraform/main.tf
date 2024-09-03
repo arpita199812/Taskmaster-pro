626686b64ab27c30f79377a0775025df9a930cba
Arpita Singh <158558110+arpita199812@users.noreply.github.com>
2024-09-03 16:05:43 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -112,28 +112,3 @@ resource "aws_instance" "my_instance" {
     Name = "my-ec2-instance"
   }
 }
-
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "subnet_id" {
-  value = aws_subnet.main.id
-}
-
-output "internet_gateway_id" {
-  value = aws_internet_gateway.main.id
-}
-
-output "route_table_id" {
-  value = aws_route_table.main.id
-}
-
-output "eks_cluster_name" {
-  value = aws_eks_cluster.main.name
-}
-
-output "ec2_instance_id" {
-  value = aws_instance.my_instance.id
-}
-
