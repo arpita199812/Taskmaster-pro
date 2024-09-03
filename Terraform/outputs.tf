a522ceb786949dc0350a3e74df8cb65dbaa7e663
Arpita Singh <158558110+arpita199812@users.noreply.github.com>
2024-09-03 23:53:55 +0530

    Update outputs.tf

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,23 +1,15 @@
+output "cluster_id" {
+  value = aws_eks_cluster.taskpro.id
+}
+
+output "node_group_id" {
+  value = aws_eks_node_group.taskpro.id
+}
+
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value = aws_vpc.taskpro_vpc.id
 }
 
-output "subnet_id" {
-  value = aws_subnet.main.id
-}
-
-output "internet_gateway_id" {
-  value = aws_internet_gateway.main.id
-}
-
-output "route_table_id" {
-  value = aws_route_table.main.id
-}
-
-output "eks_cluster_name" {
-  value = aws_eks_cluster.main.name
-}
-
-output "ec2_instance_id" {
-  value = aws_instance.my_instance.id
+output "subnet_ids" {
+  value = aws_subnet.taskpro_subnet[*].id
 }
