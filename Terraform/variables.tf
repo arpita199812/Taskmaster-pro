5e09749f231c9db5d975c00f0b69cd9435782359
Arpita Singh <158558110+arpita199812@users.noreply.github.com>
2024-09-03 23:54:51 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,45 +1,5 @@
-variable "region" {
-  description = "The AWS region to deploy to"
+variable "ssh_key_name" {
+  description = "The name of the SSH key pair to use for instances"
   type        = string
-}
-
-variable "vpc_cidr_block" {
-  description = "CIDR block for the VPC"
-  type        = string
-}
-
-variable "subnet_cidr_block" {
-  description = "CIDR block for the main subnet"
-  type        = string
-}
-
-variable "availability_zone" {
-  description = "Availability zone for the subnets"
-  type        = string
-}
-
-
-variable "eks_cluster_name" {
-  description = "Name of the EKS cluster"
-  type        = string
-}
-
-variable "eks_version" {
-  description = "EKS cluster version"
-  type        = string
-}
-
-variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
-  type        = string
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-}
-
-variable "key_name" {
-  description = "Key name for the EC2 instance"
-  type        = string
+  default     = "taskpro"
 }
