3849f96712584b955c17e88625c053191ed397c4
Arpita Singh <158558110+arpita199812@users.noreply.github.com>
2024-09-03 18:00:12 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,50 +1,39 @@
 variable "region" {
-  description = "The AWS region to create resources in"
+  description = "The AWS region to deploy to"
   type        = string
-  default     = "us-east-1"
 }
 
 variable "vpc_cidr_block" {
   description = "CIDR block for the VPC"
   type        = string
-  default     = "10.0.0.0/16"
 }
 
 variable "subnet_cidr_block" {
   description = "CIDR block for the main subnet"
-  type        = string
-  default     = "10.0.1.0/24"
-}
-
-variable "availability_zone" {
-  description = "Availability zone for the main subnet"
   type        = string
 }
 
 variable "eks_cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "my-eks-cluster"
 }
 
 variable "eks_version" {
-  description = "Version of the EKS cluster"
+  description = "EKS cluster version"
   type        = string
-  default     = "1.24"
 }
 
 variable "ami_id" {
-  description = "The AMI ID for the EC2 instance"
+  description = "AMI ID for the EC2 instance"
   type        = string
 }
 
 variable "instance_type" {
-  description = "The instance type for the EC2 instance"
+  description = "EC2 instance type"
   type        = string
-  default     = "t2.medium"
 }
 
 variable "key_name" {
-  description = "The key name for the EC2 instance"
+  description = "Key name for the EC2 instance"
   type        = string
 }
